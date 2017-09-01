a74868942b5ebcb0c3de3ae343aa8d1b1c9b627e
Flavio Moreira <fhnmor21@gmail.com>
2017-09-01 13:27:35 +1000

    added a few more tests. TODO: add support for extra arguments passing methods

--- curry_4.cpp
+++ curry_4.cpp
@@ -5,24 +5,50 @@
 
 
 // test function
+int add1 (double a)
+{
+  return a+1;
+}
+
 int add2 (int a, int b)
 {
-    return a+b;
+  return a+b;
 }
 
 int add3 (int a, int b, int c)
 {
-    return a+b+c;
+  return a+b+c;
 }
 
 int add4 (int a, float b, long c, double d)
 {
+  return a+b+c+d;
+}
+
+int add5 (int a, float b, long c, double d, bool cond)
+{
+  if(cond)
+  {
     return a+b+c+d;
+  }
+  else
+  {
+    return 0;
+  }
+}
+
+int add6 (int a, float b, long c, double d, bool cond, std::string e)
+{
+  if(!cond)
+  {
+    std::cerr << e << std::endl;
+  }
+  return a+b+c+d;
 }
 
 
-// ===
 
+// ===
 template<size_t level, size_t argsNum, typename Func, typename Args, typename R>
 struct Closure
 {
@@ -120,19 +146,35 @@ struct Curry
 
 };
 
-template< typename R, typename... As >
-auto curry(R (*f) (As... as))
+// needs a minimum of 2 arguments
+template< typename R, typename A1, typename A2, typename... As >
+auto curry(R (*f) (A1 a1, A2 a2, As... as))
 {
-  return Curry< R, As... >( f);
+  return Curry< R, A1, A2, As... >(f);
 }
 
 
 int main()
 {
-    auto f4 = curry(add4);
-    std::cout << f4(4)(3)(2)(1) << std::endl;
-    auto f3 = curry(add3);
-    std::cout << f3(4)(3)(2) << std::endl;
-    auto f2 = curry(add2);
-    std::cout << f2(4)(3) << std::endl;
+  double  val = 1;
+  const std::string msg("Hello Curry!\n");
+
+  // TODO: need to implement other ways of argument parsing, 
+  // currently only support pass by value...
+  // pointer reveference of rvalue do not work!!!
+
+  auto f6 = curry(add6);
+  std::cout << f6(4)(3)(2)(val)(false)(msg) << std::endl;
+  auto f5 = curry(add5);
+  std::cout << f5(4)(3)(2)(val)(true) << std::endl;
+  auto f4 = curry(add4);
+  std::cout << f4(4)(3)(2)(val) << std::endl;
+  auto f3 = curry(add3);
+  std::cout << f3(4)(3)(2) << std::endl;
+  auto f2 = curry(add2);
+  std::cout << f2(4)(3) << std::endl;
+
+  // NOTE: this should not work because currying needs at least 2 arguments
+  // auto f1 = curry(add1);
+  // std::cout << f1(4) << std::endl;    
 }
