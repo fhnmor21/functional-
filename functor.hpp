eb89abe4fcb76d821ba7c8416c9f63f000f5282f
Flavio Moreira <fhnmor21@gmail.com>
2017-09-06 16:54:14 +1000

    adding some more meat to functors and applicatives

--- functor.hpp
+++ functor.hpp
@@ -6,28 +6,36 @@
 namespace FunctionalCpp
 {
 
+  // forward declaration
   template <typename F>
   struct functor_wrapper;
 
-  template <typename F>
-  struct applicative_wrapper;
+  // template <typename F>
+  // struct applicative_wrapper;
 
+  // Applicative types
+  template <typename F, typename R, typename A1, typename... Args >
+  struct Applicative
+  {
+    using Fn = typename Curry<R, Args>;
+    using Ret = typename Curry<R, A1, Args>::ReturnT;
+    //using Type = applicative_wrapper<F>;
+  };
+
+  // Functor types and functions
   template <typename F>
   struct Functor
   {
-    using FunctorT = functor_wrapper<F>;
-    using FunctorFn = std::function<FunctorT::type(FunctorT::type)>;
+    // Types
+    using Type = functor_wrapper<F>;
+    using Fn = std::function<T::type(T::type)>;
 
-    template <typename... Args>
-    using applicativeR = std::function<>;
+    // simple functor map
+    static Type&& map(Fn a2b_, Type& fa_);
 
-    template <typename Arg, typename... Args>
-    using ApplicativeF = std::function<>;
-
-    static FunctorT&& map(FuntoorT& functor_, std::function<::type(T::type)>);
-    template<typename... Args>
-
-    static F&& map(F& functor_, std::function<T::type(T::type, Args...)>);
+    // applicative functor map
+    template <typename R, typename A1, typename... Args >
+    static Applicative<F, F, A1, Args>::Ret&& map(Applicative<F, F, A1, Args>::Fn a2b_, Type& fa_);
   };
 
 } // end namespace FunctionalCpp
