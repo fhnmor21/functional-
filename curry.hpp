a50ed3a6f64bbc8139ec316d3caf1276b37c243b
Flavio Moreira <fhnmor21@gmail.com>
2017-09-11 10:05:43 +1000

    improved version of curryinf function based on: andre-bergner/funky

--- curry.hpp
+++ curry.hpp
@@ -3,257 +3,77 @@
 
 #include <memory>
 #include "invoke.hpp"
-#include <iostream>
+
 
 namespace FunctionalCpp
 {
 
+  // BASED ON: www.github.com/andre-bergner/funky
 
-  // Partial Function class used to hold partial applications
-  // *******************************************************************
-  template<size_t level, size_t argsNum, typename Func, typename ArgPtrs, typename ArgVals, typename R>
+  // forward declarion of currying function
+  template < typename Function , typename... Args >
+  auto curry( Function&& f , Args&&... args );
+
+  // partial application object function
+  template < typename Function , typename... OlgArgs >
   struct PartialFn
   {
-    static std::size_t constexpr index = (argsNum - level);
-    using ReturnT = PartialFn<level-1, argsNum, Func, ArgPtrs, ArgVals, R>;
-    using ArgT = typename std::remove_pointer< typename std::tuple_element< index, ArgPtrs >::type >::type;
-
-    // CTOR
-    PartialFn(Func func)
-      : partialFn_p(new ReturnT(func))
-      , argPtrs_p(partialFn_p->argPtrs_p)
-      , argVals_p(partialFn_p->argVals_p)
-    {}
-
-    ReturnT operator()(ArgT& an)
-    {
-      std::get<index>(*argPtrs_p) = &an;
-      return (*partialFn_p.release());
-    }
-
-    ReturnT operator()(ArgT&& an)
-    {
-      auto newVal = std::make_unique<ArgT>(std::move(an));
-      auto val = std::get<index>(*argVals_p);
-      val.swap(newVal);
-      auto ptr = std::get<index>(*argPtrs_p);
-      ptr = val.get();
-
-      return (*partialFn_p.release());
-    }
-
-    // Data
-    std::unique_ptr< ReturnT > partialFn_p;
-    ArgPtrs* argPtrs_p;
-    ArgVals* argVals_p;
-  };
-
-  // Partial Function specialization for the last argument
-  // *******************************************************************
-  template<size_t argsNum, typename Func, typename ArgPtrs, typename ArgVals, typename R>
-  struct PartialFn<1, argsNum, Func, ArgPtrs, ArgVals, R>
-  {
-    static std::size_t constexpr index = (argsNum - 1);
-    using ReturnT = R;
-    using ArgT = typename std::remove_pointer< typename std::tuple_element< index, ArgPtrs >::type >::type;
-
-    // CTOR
-    PartialFn(Func func)
-      : func_m(func)
-        //, argPtrs_m(new ArgPtrs)
-        //, argVals_m(new ArgVals)
-        //, argPtrs_p(argPtrs_m.get())
-        //, argVals_p(argVals_m.get())
-    {
-      argPtrs_m.reset(new ArgPtrs);
-      argVals_m.reset(new ArgVals);
-      argPtrs_p = argPtrs_m.get();
-      argVals_p = argVals_m.get();
-    }
-
-    ReturnT operator()(ArgT& an)
-    {
-      std::get<index>(*argPtrs_m) = &an;
-
-      return Tuple::Ptrs::invoke(func_m, *argPtrs_p);
-    }
-
-    ReturnT operator()(ArgT&& an)
-    {
-      auto newVal = std::make_unique<ArgT>(std::move(an));
-      std::get<index>(*argVals_m).swap(newVal);
-      std::get<index>(*argPtrs_m) = std::get<index>(*argVals_m).get();
-
-      return Tuple::Ptrs::invoke(func_m, *argPtrs_p);
-    }
-
-    // Data
-    Func func_m;
-    std::unique_ptr< ArgPtrs > argPtrs_m;
-    std::unique_ptr< ArgVals > argVals_m;
-    ArgPtrs* argPtrs_p;
-    ArgVals* argVals_p;
-  };
-
-    // Curry type holds the recursive pratial applications to a N-ary function
-  // *******************************************************************
-  template < typename R, typename... Args >
-  struct Curry
-  {
-    static std::size_t constexpr argsNum = sizeof... (Args);
-
-    using Func = std::function<R(Args...)>;
-    using ArgPtrs = std::tuple< typename std::add_pointer< typename std::remove_reference<Args>::type>::type... >;
-    using ArgVals = std::tuple< typename std::unique_ptr< typename std::remove_reference<Args>::type>... >;
-    using ReturnT = PartialFn<argsNum, argsNum, Func, ArgPtrs, ArgVals, R>;
-
-    static ReturnT&& Factory(Func func)
-    {
-      auto r = ReturnT(std::forward<Func>(func));
-      return std::move(r);
-    }
-
-    // CTORS
-    Curry() = delete;
-    Curry(Curry&) = delete;
   private:
-    Curry(Func func){}
-
-  };
-
-
-
-
-  ////////////////////////////////////////////////////////////////////////
-  ////////////////////////////////////////////////////////////////////////
-  template< size_t level, typename Func, typename ArgVals, typename R >
-  struct PartialFunc
-  {
-    // Types
-    static std::size_t constexpr argsNum = std::tuple_size<typename std::remove_reference<ArgVals>::type>::value;
-    static std::size_t constexpr index = (argsNum - level);
-    using ArgT = typename std::tuple_element< index, ArgVals >::type::element_type;
-    // using ArgT = typename std::remove_pointer< typename std::tuple_element< index, ArgPtrs >::type >::type;
-    using ReturnT = PartialFunc<level-1, Func, ArgVals, R>;
-
-    // Ctor
-    PartialFunc(Func func)
-      : partialFunc_m(new ReturnT(func))
-    {
-      std::cerr << "PartialFunc CTOR > " << argsNum << " - " << level << " @ " << this << std::endl;
-    }
-
-    PartialFunc(PartialFunc&) = default;
-    PartialFunc(PartialFunc&&) = default;
-
-
-    ~PartialFunc()
-    {
-      std::cerr << "PartialFunc DTOR < " << argsNum << " - " << level << " @ " << this << std::endl;
-    }
-
-    //ReturnT operator()(ArgT& an)
-    //{
-    //  return partialFunc_m;
-    //}
-
-    auto operator()(ArgT&& an)
-    {
-      std::cerr << "Operator () > " << argsNum << " - " << level << " arg: " << an << " @ " << this << std::endl;
-      return partialFunc_m->getFn();
-    }
-
-    void id()
-    {
-      std::cerr << "ID : " << this << std::endl;
-    }
-
-    std::function<ReturnT(ArgT)> getFn()
-    {
-      return std::function<ReturnT(ArgT)>(this);
-    }
 
     // Data
-    ReturnT* partialFunc_m;
-  };
+    Function const & func_;
+    std::tuple< typename std::remove_reference<OlgArgs>::type... >   args_;
 
-  template< typename Func, typename ArgVals, typename R >
-  struct PartialFunc<1, Func, ArgVals, R>
-  {
-    // Types
-    static std::size_t constexpr argsNum = std::tuple_size<typename std::remove_reference<ArgVals>::type>::value;
-    static std::size_t constexpr index = (argsNum - 1);
-    using ArgT = typename std::tuple_element< index, ArgVals >::type::element_type;
-    using ReturnT = R;
+
+    // SFINAE trick to deal with incomplete set of arguments
+    struct call_failed {};
+    struct call_succeed : call_failed {};
+
+    // dispacther function for correct numbers of arguments
+    template < typename... Args >
+    auto dispatch( call_succeed , Args&&... args ) const -> decltype( func_(args...) )
+    {
+      return  func_( std::forward<Args>(args)... );
+    }
+
+    // dispatch function for incomplete set of arguments
+    template < typename... Args >
+    auto dispatch( call_failed , Args&&... args ) const -> decltype( curry( func_ , std::forward<Args>(args)... ) )
+    {
+      return  curry( func_ , std::forward<Args>(args)... );
+    }
+
+    // calling to the function forwarding arguments
+    template < std::size_t... Ns , typename... Other_args >
+    auto call( std::index_sequence<Ns...> , Other_args&&... other_args )
+    {
+      return dispatch( call_succeed{} , std::get<Ns>(args_)... , std::forward<Other_args>(other_args)... );
+    }
+
+  public:
 
     // Ctor
-    PartialFunc(Func func)
-      : func_m(func)
+    PartialFn( Function const &f , OlgArgs&&... as )
+    : func_(f) , args_( std::forward<OlgArgs>(as)...) {}
+
+    // function object operator
+    template < typename NewArg >
+    auto operator()( NewArg&& newArg ) 
     {
-      std::cerr << "PartialFunc CTOR > " << argsNum << " - 1 @ " << this << std::endl;
+      return  call( std::make_index_sequence<sizeof...(OlgArgs)>()
+                  , std::forward<NewArg>(newArg) );
     }
 
-    PartialFunc(PartialFunc&) = default;
-    PartialFunc(PartialFunc&&) = default;
-
-
-    ~PartialFunc()
-    {
-      std::cerr << "PartialFunc DTOR < " << argsNum << " - 1 @ " << this << std::endl;
-    }
-
-    //Func operator()(ArgT& an)
-    //{
-    //  return func_m;
-    //}
-
-    Func&& operator()(ArgT&& an)
-    {
-      std::cerr << "Operator () > " << argsNum << " - 1" << " arg: " << an << " @ " << this << std::endl;
-      return std::move(func_m);
-    }
-
-    std::function<ReturnT(ArgT)> getFn()
-    {
-      return std::function<ReturnT(ArgT)>(this);
-    }
-
-    void id()
-    {
-      std::cerr << "ID : " << this << std::endl;
-    }
-
-
-    // Data
-    Func func_m;
-    //ArgVals argVals_m;
   };
 
-
-  // given a function will return a curry structure
-  // needs a minimum of 2 arguments
-  // template< typename R, typename A1, typename A2, typename... Args >
-  // auto curry(R (*f) (A1, A2, Args... as))
-  template< typename R, typename... Args >
-  auto curry(R (*f) (Args... as))
+  // currying function
+  template< typename Function , typename... Args >
+  auto curry( Function&& f , Args&&... boundArgs )
   {
-    static std::size_t constexpr argsNum = sizeof... (Args);
-
-    using Func = std::function<R(Args...)>;
-    using ArgVals = std::tuple< typename std::unique_ptr< typename std::remove_reference<Args>::type>... >;
-    using ReturnT = PartialFunc<argsNum, Func, ArgVals, R>;
-
-    ReturnT* partialFunc = new ReturnT(std::function<R(Args...)>(f));
-    partialFunc->id();
-
-    return (*partialFunc);
-
-    //return std::move(ReturnT(std::function<R(Args...)>(f)) );
-
-
-    //return Curry<R, A1, A2, Args...>::Factory(std::function<R(A1, A2, Args...)>(f));
+    return PartialFn<Function, Args...>( f , std::forward<Args>(boundArgs)... );
   }
 
 } // end nasmespace FunctionalCpp
 
 #endif
+
--- test.cpp
+++ test.cpp
@@ -1,7 +1,7 @@
 #include <cstddef>
 #include <iostream>
 #include "curry.hpp"
-#include "functor.hpp"
+//#include "functor.hpp"
 
 using namespace FunctionalCpp;
 
@@ -50,35 +50,44 @@ int add6 (int a, float b, long c, double& d, bool cond, const std::string& e)
 // testing
 int main()
 {
-  auto f = [](int a){return a;};
-  auto f_t = Curry<int, int>::Factory(std::function<int(int)>(f));
-  std::cout << f_t(4) << std::endl;
 
-  // double  val = 1;
-  // std::string msg("Hello Curry!\n");
+  auto f_t2 = curry(add2);
+  auto f_t2_ = f_t2(4);
+  std::cerr << "f_t2 " << f_t2_(5) << std::endl;
 
-  // auto f6 = curry(add6);
-  // std::cout << f6(4)(3)(2)(val)(false)(msg) << std::endl;
+  auto f_t3 = curry(add3);
+  auto f_t3_ = f_t3(4);
+  std::cerr << "f_t3 " << f_t3_(5)(6) << std::endl;
 
-  // auto f5 = curry(add5);
-  // std::cout << f5(4)(3)(2)(val)(true) << std::endl;
+  double  val = 10;
+  auto f_t4 = curry(add4);
+  auto f_t4_ = f_t4(4);
+  std::cerr << "f_t4 " << f_t4_(5)(6)(val) << std::endl;
 
-  // auto f4 = curry(add4);
-  // std::cout << f4(4)(3)(2)(val) << std::endl;
+  std::string msg("Hello Curry!\n");
 
-  // auto f3 = curry(add3);
-  // std::cout << f3(4)(3)(2) << std::endl;
+  auto f6 = curry(add6);
+  std::cout << "f6 " << f6(4)(3)(2)(val)(false)(msg) << std::endl;
 
-  // int val_3 = 3;
-  // auto f2 = curry(add2);
-  // std::cout << f2(val_3)(4) << std::endl;
+  auto f5 = curry(add5);
+  std::cout << "f5 " << f5(4)(3)(2)(val)(true) << std::endl;
+
+  auto f4 = curry(add4);
+  std::cout << "f4 " << f4(4)(3)(2)(val) << std::endl;
+
+  auto f3 = curry(add3);
+  std::cout << "f3 " << f3(4)(3)(2) << std::endl;
+
+  int val_3 = 3;
+  auto f2 = curry(add2);
+  std::cout << "f2 " << f2(val_3)(4) << std::endl;
 
   // auto plus3 = f2(3);
   // std::vector<int> input{0,1,2,3,4,5,6,7,8,9};
   // auto output = fmap(plus3, input);
   // for(auto& o: output)
   // {
-  //   std::cout << 
+  //   std::cout << "Functor" ;
   // }
 
   // NOTE: this should not work because currying needs at least 2 arguments
