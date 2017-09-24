a7f47ad18099eba7e7e1fff4c1038c6b7bd5e66b
Flavio Moreira <fhnmor21@gmail.com>
2017-09-25 09:57:54 +1000

    WIP- implementaing Applicative

--- functor.hpp
+++ functor.hpp
@@ -7,60 +7,54 @@
 namespace FunctionalCpp
 {
 
-  template <
-    typename F,
-    typename R,
-    typename A1 >
+  // functor wrapper (static class)
+  template < typename F,
+             typename R,
+             typename A1 >
   struct Functor
   {
     using Function = std::function<R(A1)>;
-    using Result = R;
-    using Value = A1; // this should be the same as value
-    using iFunctor = F;//<Value>;
-    using oFunctor = typename rebind_type<F, Result>::type;
+    using iFunctor = F; //<Value>;
+    using oFunctor = typename rebind_type<F, R>::type;
 
     static auto map(const typename Functor<F, R, A1>::Function& a2b, const typename Functor<F, R, A1>::iFunctor& fa){}
   };
 
-  // empty implementation
+  // Functor empty implementation
   // has to be overriden by every type of functor
-  template <
-    typename F,
-    typename R,
-    typename A1 >
+  template < typename F,
+             typename R,
+             typename A1 >
   struct FunctorImpl : Functor<F, R, A1>
   {};
 
 
   // template function
-  template <
-    typename F,
-    typename R,
-    typename A1 >
-  //auto fmap(typename Functor<F, R, A1>::Function a2b, typename Functor<F, R, A1>::iFunctor fa)
+  template < typename F,
+             typename R,
+             typename A1 >
   auto fmap(const std::function<R(A1)>& a2b, F& fa)
   {
     return FunctorImpl<F, R, A1>::map(a2b, fa);
   }
 
 
-  // template <typename F, typename R, typename A1, typename A2, typename... As>
-  // struct ApplicativeTypes
-  // {
-  //   using function = Curry<R, A1, A2, As...>;
-  //   using result = function_type::ReturnT;
-  //   using arg = A1; // this should be the same as value_type
-  //   using value = inner_type<F>; // this should be the same as arg_type
-  //   using = std::is_same<arg, value>::value;
+  // applicative wrapper (static class)
+  template < typename F,
+             typename R,
+             typename A1,
+             typename A2,
+             typename... As>
+  struct Applicative : Functor<F, R, A1>
+  {
+    using Function  = typename Return<0,R, A1, A2, As...>::type;
+    using Result = typename Return<0, R, A2, As...>::type;
+    using aFunctor = typename rebind_type<F, Result>::type;
 
-  //   using ifunctor = F;
-  //   using ofunctor = rebind_type<F, result>;
-  // };
+    static auto map(const typename Applicative<F, R, A1, A2, As...>::Function& a2b, const typename Applicative<F, R, A1, A2, As...>::iFunctor& fa){}
+  };
 
-  // // applicative functor map
-  // template <typename F, typename R, typename A1, typename A2, typename... As>
-  // ApplicativeTypes<F, R, A1, A2, As...>::ofunctor&&
-  // fmap(ApplicativeTypes<F, R, A1, A2, As...>::function a2b_, ApplicativeTypes<F, R, A1, A2, As...>::ifunctor& fa_ );
+
 
 } // end namespace FunctionalCpp
 
