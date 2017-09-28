6af10b1fe1db6845eb8cba468e0977be678a8131
Flavio Moreira <fhnmor21@gmail.com>
2017-09-28 16:18:37 +1000

    formating arrow.hpp

--- arrow.hpp
+++ arrow.hpp
@@ -6,48 +6,49 @@
 
 namespace FunctionalCpp
 {
-template <typename A, typename B>
-using Function = std::function<B(A)>;
+  template <typename A, typename B>
+  // struct Function : std::function<B(A)>{};
+  using Function = std::function<B(A)>;
 
-namespace Impl_
-{
-  // **************************************************
-  // recursive std::function return type for partial applications
-  template < size_t N,
-             typename Ret,
-             typename... Args >
-  struct Nested
-  {};
-
-  template < size_t N,
-             typename Ret,
-             typename Arg,
-             typename... Args >
-  struct Nested< N, Ret, Arg, Args... > : Nested< N-1, Ret, Args... >
-  {};
-
-  template < typename Ret,
-             typename Arg,
-             typename... Args >
-  struct Nested< 0, Ret, Arg, Args... >
+  namespace Impl_
   {
-  private:
-  using R = typename Nested< 0, Ret, Args... >::type;
-  public:
-  using type = Function<Arg, R>;
-  };
+    // **************************************************
+    // recursive std::function return type for partial applications
+    template < size_t N,
+              typename Ret,
+              typename... Args >
+    struct Nested
+    {};
 
-  template < typename Ret >
-  struct Nested< 0, Ret >
-  {
-  public:
-  using type = Ret;
-  };
-  } // end namespace Impl_
+    template < size_t N,
+              typename Ret,
+              typename Arg,
+              typename... Args >
+    struct Nested< N, Ret, Arg, Args... > : Nested< N-1, Ret, Args... >
+    {};
 
-  template < typename Ret,
-             typename... Args>
-  using Curried = Impl_::Nested<0, Ret, Args...>;
+    template < typename Ret,
+              typename Arg,
+              typename... Args >
+    struct Nested< 0, Ret, Arg, Args... >
+    {
+    private:
+    using R = typename Nested< 0, Ret, Args... >::type;
+    public:
+    using type = Function<Arg, R>;
+    };
+
+    template < typename Ret >
+    struct Nested< 0, Ret >
+    {
+    public:
+    using type = Ret;
+    };
+    } // end namespace Impl_
+
+    template < typename Ret,
+              typename... Args>
+    using Curried = Impl_::Nested<0, Ret, Args...>;
 
 
 } // end namespace FunctionalCpp
