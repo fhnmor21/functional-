94b1d031b7426938b9745b4bc83c8445e726811d
Flavio Moreira <fhnmor21@gmail.com>
2017-10-04 12:01:49 +1100

    added tuple swap to invoke

--- invoke.hpp
+++ invoke.hpp
@@ -12,6 +12,35 @@ namespace FunctionalCpp
   namespace Tuple
   {
 
+    template<class A1, class A2, class... As>
+    struct Swapper
+    {
+    private:
+      static std::tuple<A2, A1, As...>  swap_impl2(A1 a1, A2 a2, As... as)
+      {
+        return std::move(std::tuple< A2, A1, typename std::remove_reference<As>::type... >(a2, a1, as...));
+      }
+      
+      template <size_t ...S >
+      static std::tuple<A2, A1, As...> swap_impl1(std::tuple<A1, A2, As...> from, std::index_sequence<S...> )
+      {
+        return Swapper<A1, A2, As...>::swap_impl2(std::get<S>(std::forward< std::tuple<A1, A2, As...> >(from))...);
+      }
+      
+    public:
+      static std::tuple<A2, A1, As...>  swap(std::tuple<A1, A2, As...> from)
+      {
+        size_t constexpr tSize = std::tuple_size<typename std::remove_reference< std::tuple<A1, A2, As...> >::type>::value;
+        return Swapper<A1, A2, As...>::swap_impl1( from, std::make_index_sequence<tSize>() );
+      }
+    };
+    
+    template<class A1, class A2, class... As>
+    std::tuple<A2, A1, As...>  swap(std::tuple<A1, A2, As...> from)
+    {
+      return Swapper<A1, A2, As...>::swap(from);
+    }
+
     namespace Vals
     {
       // tuple apply - SOURCE: http://www.cppsamples.com/common-tasks/apply-tuple-to-function.html
