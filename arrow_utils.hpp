ab0eeb82ecf1b12a6f2d3190c4ec649c7846c729
Flavio Moreira <fhnmor21@gmail.com>
2017-10-05 18:09:55 +1100

    Arrow initial version

--- arrow_utils.hpp
+++ arrow_utils.hpp
@@ -16,6 +16,17 @@ namespace FunctionalCpp
     return Arrow<A, B, C, D>::prod3( a2c, b2d );
   }
 
+  template < class A,
+             class B,
+             class C,
+             class D >
+  auto prod3( C(a2c)(A), D(b2d)(B) )
+  {
+    Function1<A, C> a2c_ = a2c;
+    Function1<B, D> b2d_ = b2d;
+    return prod3( a2c_, b2d_ );
+  }
+
   // (&&&) :: (a `arr` c) -> (a `arr` d) -> (a `arr` (c, d))
   template < class A,
              class C,
@@ -28,6 +39,16 @@ namespace FunctionalCpp
       return Arrow<A, A, C, D>::prod3( a2c, a2d )(aa);
     };
     return std::move(a2cd);
+  }
+
+  template < class A,
+             class C,
+             class D >
+  auto and3( C(a2c)(A), D(a2d)(A) )
+  {
+    Function1<A, C> a2c_ = a2c;
+    Function1<A, D> a2d_ = a2d;
+    return and3( a2c_, a2d_ );
   }
 
   // ===
@@ -59,6 +80,14 @@ namespace FunctionalCpp
     return std::move( First<A, C>(a2c) );
   }
 
+  template < class A,
+             class C >
+  auto first( C(a2c)(A) )
+  {
+    Function1<A, C> a2c_ = a2c;
+    return first( a2c_ );
+  }
+
   // ===
   // class to encapsulate: second :: (c `arr` d) -> ((b, c) `arr` (b, d))
   // NOTE: needed to delay the deduction of the last argument <class B>
@@ -86,6 +115,14 @@ namespace FunctionalCpp
   auto second( Function1<C, D> c2d )
   {
     return std::move( Second<C,D>(c2d) );
+  }
+
+  template < class C,
+             class D >
+  auto second( D(c2d)(C) )
+  {
+    Function1<C, D> c2d_ = c2d;
+    return second( c2d );
   }
 
 } // end namespace FunctionalCpp
