b159bd0ec46e1c8918bac25ec121e25a85bc7140
Flavio Moreira <fhnmor21@gmail.com>
2017-10-06 00:24:51 +1100

    remove dead code from dict

--- dict.hpp
+++ dict.hpp
@@ -10,108 +10,9 @@ namespace FunctionalCpp
   // ****************************************************************
   // local implementations / instatiations for STL containers
 
-  /*
-  struct Atom
-  {
-    std::string data;
-    const bool operator <( const Atom& rhs ) const
-    {
-      return data < rhs.data;
-    }
-    const bool operator >( const Atom& rhs ) const
-    {
-      return data > rhs.data;
-    }
-  };
-  */
-
-  // FIXME: remove the need to convert to string!!!
-  /// works for now. not high priority...
-  class Atom
-  {
-  public:
-    enum class atomTypes
-    {
-      String,
-      Integer,
-      Number,
-    };
-
-    Atom() = delete;
-    Atom(const Atom& other);
-    explicit Atom(const std::string& s);
-    explicit Atom(const char* c);
-    // explicit Atom(long long i);
-    explicit Atom(double d);
-
-    const Atom& operator=(const Atom& rhs)
-    {
-      data_m = rhs.data_m;
-      option_m = rhs.option_m;
-      return *this;
-    }
-
-    const bool operator<(const Atom& rhs) const { return data_m < rhs.data_m; }
-
-    const bool operator>(const Atom& rhs) const { return data_m > rhs.data_m; }
-
-    const std::string& value() const { return data_m; }
-
-  private:
-    atomTypes option_m;
-    std::string data_m;
-  };
-
-  Atom::Atom(const Atom& other)
-      : option_m(other.option_m)
-      , data_m(other.data_m)
-  {
-  }
-
-  Atom::Atom(const std::string& s)
-      : option_m(atomTypes::String)
-      , data_m(s)
-  {
-  }
-
-  Atom::Atom(const char* c)
-      : option_m(atomTypes::String)
-      , data_m(c)
-  {
-  }
-
-  /*
-  Atom::Atom(long long i)
-    : option_m(atomTypes::Integer)
-  {
-    data_m = std::to_string(i);
-  }
-  */
-
-  Atom::Atom(double d)
-      : option_m(atomTypes::Number)
-  {
-    data_m = std::to_string(d);
-  }
-
-  template < class V >
-  struct ADict
-  {
-    std::map< const Atom, V > data;
-    const V& operator[](int k) { return data[Atom{static_cast< double >(k)}]; }
-    const V& operator[](double k) { return data[Atom{k}]; }
-    const V& operator[](const std::string& k) { return data[Atom{k}]; }
-    const V& operator[](const char* k) { return data[Atom{k}]; }
-  };
-
-  // template <class V>
-  // using ADict = std::map<Atom, V>;
-
   // instance for std::map
   template < class V >
-  using Dict = std::map< const std::string,
-                         V >; //, std::less<std::string>,
-                              // std::allocator<std::pair<const std::string, V>>;
+  using Dict = std::map< const std::string, V >;
 
   template < class Ret, class Arg >
   struct Functor< Function1< Arg, Ret >, Dict< Arg >, Dict< Ret > >
@@ -122,13 +23,14 @@ namespace FunctionalCpp
 
       for(auto& oldVal : fa)
       {
-        auto newVal = a2b(oldVal);
-        fb.push_back(newVal);
+        auto newVal = a2b(oldVal.second);
+        fb[oldVal.first].newVal;
       }
       return std::move(fb);
     }
   };
 
+  /* TBD ???
   template < class Ret, class Arg >
   struct Applicative< Dict< Function1< Arg, Ret > >, Dict< Arg >, Dict< Ret > >
   {
@@ -140,13 +42,14 @@ namespace FunctionalCpp
       {
         for(auto& oldVal : fa)
         {
-          auto newVal = a2b(oldVal);
-          fb.push_back(newVal);
+          auto newVal = a2b(oldVal.second);
+          fb[oldVal.first].newVal;
         }
       }
       return std::move(fb);
     }
   };
+  */
 
 } // end namespace FuncA2BalCpp
 
